6874c8e03bd9fa32b9a8adc75e47cb9b16327068
Salvador Ruvalcaba <salvador.ruvalcaba@coppel.com>
2022-12-04 07:39:49 -0600

    fixes

--- project/__init__.py
+++ project/__init__.py
@@ -1,61 +1,52 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
-
 from fastapi import status
 from fastapi import Depends
 from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from .database import UserReview
+from .routers import user_router
+from .routers import review_router
+from .common import create_access_token
 
 from .database import User
 from .database import Movie
-from .database import UserReview
+from .Connections import ConnectionMySQL
 
-from fastapi.security import OAuth2PasswordRequestForm
-
-from .routers import user_router
-from .routers import review_router
-
-from .common import create_access_token
-
-from .database import database as connection
-
-app = FastAPI(title='Movies Review CF', 
-            description='En este proyecto seremos capaces de reseñar peliculas.', 
-            version='0.1')
+app = FastAPI(title='CuentasApp API',
+              description='Proyecto backend API CuentasApp',
+              version='1.0')
 
 api_v1 = APIRouter(prefix='/api/v1')
 
 api_v1.include_router(user_router)
 api_v1.include_router(review_router)
-
 app.include_router(api_v1)
+database_connection = ConnectionMySQL()
+
 
 @app.post('/auth')
 async def auth(data: OAuth2PasswordRequestForm = Depends()):
     user = User.authenticate(data.username, data.password)
-    
+
     if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Usuario o contraseña incorrectos",
             headers={"WWW-Authenticate": "Bearer"},
-        ) 
-
+        )
     return {
-        'access_token': create_access_token(user.id, user.username),
+        'access_token': create_access_token(user[0], user[1]),
         'token_type': 'bearer'
-    } 
+    }
 
 
 @app.on_event("startup")
 async def startup():
-    if connection.is_closed():
-        connection.connect()
-    
-    connection.create_tables([User, Movie, UserReview])
+    print("Inicio del servicio")
 
 
 @app.on_event("shutdown")
 async def startup():
-    if not connection.is_closed():
-        connection.close()
-
+    database_connection.mysql_close()
+    print("La conexión se apagó")
