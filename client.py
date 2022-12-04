674612deb1d87d06e68f359cc13c794cd3d0f571
Salvador Ruvalcaba <salvador.ruvalcaba@coppel.com>
2022-12-04 07:20:26 -0600

    cookie file client updated

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import requests
 URL = 'http://localhost:8000/api/v1/users/'
 
 USER = {
-    'username': 'eduardo_gpg',
+    'username': 'ruvalcaba',
     'password': 'password'
 }
 
@@ -14,7 +14,9 @@ if response.status_code == 200:
 
     user_id = response.cookies.get_dict().get('user_id')
 
-    cookies = { 'user_id': user_id }
+    cookies = {
+        'user_id': user_id
+    }
     response = requests.get(URL + 'reviews', cookies=cookies)
 
     if response.status_code == 200:
