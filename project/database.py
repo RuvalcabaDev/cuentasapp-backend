87f894b4da081f7878899b8db87a62a97617468d
Salvador Ruvalcaba <salvador.ruvalcaba@coppel.com>
2022-12-04 07:38:57 -0600

    fixes and authenticate define

--- project/database.py
+++ project/database.py
@@ -1,13 +1,12 @@
 import hashlib
-
 import datetime
-from peewee import *
+from dotenv import load_dotenv
+from peewee import MySQLDatabase, Model, CharField, DateTimeField, ForeignKeyField, TextField, IntegerField
+from .Connections import ConnectionMySQL
 
-database = MySQLDatabase('fastapi_project', 
-        user='root',
-        password='DATABASEPASSWORD',
-        host='localhost',
-        port=3306)
+load_dotenv()
+db_connect = ConnectionMySQL()
+SEP_RULES = " --- "
 
 
 class User(Model):
@@ -16,25 +15,27 @@ class User(Model):
     created_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
-        database = database
+        database = db_connect
         table_name = 'users'
-
 
     @classmethod
     def create_password(cls, password):
         h = hashlib.md5()
         h.update(password.encode('utf-8'))
-
         return h.hexdigest()
-
 
     @classmethod
     def authenticate(cls, username, password):
-        user = cls.select().where(User.username == username).first()
-
-        if user and user.password == User.create_password(password):
+        query = 'SELECT * FROM users WHERE username = %s;'
+        print("Intentando acceder")
+        db_connect.mysql_connect()
+        db_connect.cursor.execute(query, (username))
+        user = db_connect.cursor.fetchone()
+        db_connect.connection.commit()
+        print("Usuario existe:", user, sep=SEP_RULES)
+        db_connect.mysql_close()
+        if user and user[2] == User.create_password(password):
             return user
-
 
     def __str__(self):
         return self.username
@@ -45,7 +46,7 @@ class Movie(Model):
     created_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
-        database = database
+        database = db_connect
         table_name = 'movies'
 
 
@@ -57,7 +58,7 @@ class UserReview(Model):
     created_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
-        database = database
+        database = db_connect
         table_name = 'user_reviews'
 
     def __str__(self):
